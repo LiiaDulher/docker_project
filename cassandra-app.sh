cbbe3e0cc359701d398ced74b51699969e4ffbff
LiiaDulher <61160259+LiiaDulher@users.noreply.github.com>
2022-06-09 21:13:49 +0300

    Update cassandra-app.sh

--- cassandra-app.sh
+++ cassandra-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t cassandra_app -f Dockerfile4 .
+docker build -t cassandra_app -f Dockerfile2 .
 
 docker run --name cassandra-flask --network dulher-project-network -p 8080:8080 -d cassandra_app
