fc10ff43eee50b8373fb5231528b11d0b07424d3
LiiaDulher <61160259+LiiaDulher@users.noreply.github.com>
2022-06-09 21:14:05 +0300

    Update run-cassandra-cluster.sh

--- run-cassandra-cluster.sh
+++ run-cassandra-cluster.sh
@@ -4,5 +4,5 @@ docker network create dulher-project-network
 docker run --name cassandra-node --network dulher-project-network -p 9042:9042 -d cassandra:latest
 sleep 70s
 echo "Creating keyspace and tables"
-docker build -t ddl_image -f Dockerfile3 .
+docker build -t ddl_image -f Dockerfile1 .
 docker run -it --network dulher-project-network --rm ddl_image
